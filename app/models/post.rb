fc475fecca351846c0ff2564fe9f97b15166d384
TakuroSugahara <k5690033@gmail.com>
2020-05-15 16:06:13 +0900

    feat: postの公開情報

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  validates :title, :content, :status, presence: {message:'は、必須項目です。'}
+
+  def display_status()
+    if self.status == 1
+      return '公開'
+    end
+    return '非公開'
+  end
+
+  def unpublish_number
+    return 2
+  end
 end
